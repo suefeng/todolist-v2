0ebfa75c1e0403ea7a517b131ec5f39da05936cd
Sue Tan <suefengtan@gmail.com>
2023-05-28 14:48:15 -0500

    Modify config files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "http://localhost:3007/api/v1/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
