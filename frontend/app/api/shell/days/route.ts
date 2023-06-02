47d5ec630db986f48495b5902c05706c340e25d6
Sue Tan <suefengtan@gmail.com>
2023-06-02 18:04:49 -0500

    Add days shell

---
+++ frontend/app/api/shell/days/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+
+import { PassedOptions } from 'infrastructure/api/common';
+import { hasErrorInResponse } from 'infrastructure/api/common/hasResponseError';
+import { fetchDays } from 'infrastructure/api/gateway/shell/days/days.api';
+import { remoteAPI } from 'infrastructure/api/remotes/remoteAPI';
+import { GetGatewayFetchReturnType } from 'infrastructure/utility-types/utility-types';
+
+export async function GET() {
+  const options = {
+    log: true,
+    headers: {},
+  } as PassedOptions;
+
+  const days = await remoteAPI.days.fetchDay({}, options);
+
+  if (hasErrorInResponse(days)) {
+    return NextResponse.json(
+      {
+        error: days.error,
+      },
+      { status: 400 },
+    );
+  }
+
+  const response: HandlerResponse = {
+    data: {
+      ...days.data,
+    },
+    error: null,
+  };
+
+  return NextResponse.json(response, { status: 200 });
+}
+
+export type HandlerResponse = GetGatewayFetchReturnType<typeof fetchDays>;
